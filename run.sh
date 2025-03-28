710bc5c409ca45ad4aff2ff11289ff591ba3df37
LuoJianPing <812184246@qq.com>
2025-03-28 20:03:42 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,18 +1,10 @@
-CUDA_VISIBLE_DEVICES=0 python3 test_NTIRE.py \
-    --data_dir test_imgs_dir \
-    --mode hybrid_test \
-    --model mambair \
-    --model_dir model_zoo/team02_Mambair_30.367.pth \
-    --tile 320 \
-    --tile_overlap 32 \
-    --out_dir Your_path 
+CUDA_VISIBLE_DEVICES=0 python3 test_demo.py
+--data_dir ./NTIRE2025_Challenge/input
+--model_id 2
+--save_dir ./NTIRE2025_Challenge/results
 
-CUDA_VISIBLE_DEVICES=0 python3 test_NTIRE.py \
-    --data_dir test_imgs_dir\
-    --mode hybrid_test \
-    --model promptir \
-    --model_dir model_zoo/team02_Promptir_Dn50.pth \
-    --tile 192 \
-    --tile_overlap 32 \
-    --out_dir Your_path
+CUDA_VISIBLE_DEVICES=0 python3 test_demo.py
+--data_dir ./NTIRE2025_Challenge/input
+--model_id 2
+--save_dir ./NTIRE2025_Challenge/results
 
